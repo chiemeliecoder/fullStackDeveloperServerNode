2d2b6ee81b0d4607eb075cddb83ac39ebdb3a2e9
chiemeliecode <ezeokeke.c@northeastern.edu>
2022-04-06 01:34:59 -0400

    a8 REST sp22

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ TuitsController(app);
 app.get('/', (req, res) => {
   res.send('Welcome to Full Stack Development!')
 })
-app.listen(process.env.PORT ||5000);
+app.listen(process.env.PORT ||4000);
