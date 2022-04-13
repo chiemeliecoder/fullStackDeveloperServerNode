41e2b6eeb5963edb74e32d673114046060c9187f
chiemeliecode <ezeokeke.c@northeastern.edu>
2022-04-13 17:36:43 -0400

    A9 MONGO sp22

--- Tuits/TuitsDao.js
+++ Tuits/TuitsDao.js
@@ -2,8 +2,8 @@ import TuitsModel from "./TuitsModel.js";
 
 export const findAllTuits = () => TuitsModel.find();
 
-export const createTuit = (tuit) => tuitsModel.create(tuit);
+export const createTuit = (tuit) => TuitsModel.create(tuit);
 
-export const deleteTuit = (tid) => tuitsModel.deleteOne({_id: tid});
+export const deleteTuit = (tid) => TuitsModel.deleteOne({_id: tid});
 
-export const updateTuit = (tid, tuit) => tuitsModel.updateOne({_id: tid}, {$set: tuit})
+export const updateTuit = (tid, tuit) => TuitsModel.updateOne({_id: tid}, {$set: tuit})
